0b95926b30cf66bea382a868909a18a41f1d9141
Crow_93@abv.bg <Crow_93@abv.bg>
2016-01-30 16:07:32 +0200

    assets

---
+++ TerroristsRun/Assets/Scripts/move.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class move : MonoBehaviour {
+
+    public float speed;
+
+    private Rigidbody rb;
+    private Vector3 movement;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        movement = rb.transform.position;
+    }
+
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            movement = new Vector3(moveHorizontal + speed, 0.0f, moveVertical);
+            rb.AddForce(movement);
+        }
+        else if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            movement = new Vector3(moveHorizontal - speed, 0.0f, moveVertical);
+            rb.AddForce(movement);
+        }
+    }
+}
---
+++ TerroristsRun/Assets/Scripts/play_ground_looper.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class play_ground_looper : MonoBehaviour {
+
+    public GameObject player;
+    private Vector3 offset;
+    private int lengthOfBackground;
+
+	void Start ()
+    {
+        var playgroundObjects = GameObject.FindGameObjectsWithTag("playground");
+        lengthOfBackground = playgroundObjects.Length;
+        if (playgroundObjects != null && playgroundObjects.Length > 0)
+        {
+            var playgroundObject = playgroundObjects[0];
+            transform.position = new Vector3(-playgroundObject.transform.localScale.x, 0.0f, 0.0f);
+        }
+
+        offset = transform.position - player.transform.position;
+    }
+
+    void LateUpdate()
+    {
+        transform.position = player.transform.position + offset;
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("playground"))
+        {
+            var newX = lengthOfBackground * other.transform.localScale.x;
+            other.transform.position = new Vector3(other.transform.position.x + newX, 0.0f, 0.0f);
+        }
+    }
+}
